5e9b074627f1573c0b7979f6ea60310b41d5df94
Gabriel Ramos <gabrielramos791@gmail.com>
2024-07-06 19:51:41 -0300

    create blob metadata manager

--- src/BlobStorage/Entities/Blob/BlobMetadata.php
+++ src/BlobStorage/Entities/Blob/BlobMetadata.php
@@ -4,19 +4,53 @@ declare(strict_types=1);
 
 namespace Sjpereira\AzureStoragePhpSdk\BlobStorage\Entities\Blob;
 
+use DateTimeImmutable;
 use Sjpereira\AzureStoragePhpSdk\BlobStorage\Resource;
 use Sjpereira\AzureStoragePhpSdk\Exceptions\InvalidArgumentException;
 
+/**
+ * @phpstan-type BlobMetadataHeaders array{Content-Length?: string, Last-Modified?: string, ETag?: string, Vary?: string, Server?: string, x-ms-request-id?: string, x-ms-version?: string, Date?: string}
+ */
 final readonly class BlobMetadata
 {
-    /** @param array<string, scalar|null> $metadata */
-    public function __construct(public array $metadata)
+    public ?int $contentLength;
+
+    public ?DateTimeImmutable $lastModified;
+
+    public ?string $eTag;
+
+    public ?string $vary;
+
+    public ?string $server;
+
+    public ?string $xMsRequestId;
+
+    public ?string $xMsVersion;
+
+    public ?DateTimeImmutable $date;
+
+    /**
+     * @param array<string, scalar|null> $metadata
+     * @param BlobMetadataHeaders $options
+    */
+    public function __construct(public array $metadata, array $options = [])
     {
-        //
+        $this->contentLength = isset($options['Content-Length']) ? (int) $options['Content-Length'] : null;
+        $this->lastModified  = isset($options['Last-Modified']) ? new DateTimeImmutable($options['Last-Modified']) : null;
+        $this->eTag          = $options['ETag'] ?? null;
+        $this->vary          = $options['Vary'] ?? null;
+        $this->server        = $options['Server'] ?? null;
+        $this->xMsRequestId  = $options['x-ms-request-id'] ?? null;
+        $this->xMsVersion    = $options['x-ms-version'] ?? null;
+        $this->date          = isset($options['Date']) ? new DateTimeImmutable($options['Date']) : null;
     }
 
     public function get(string $name): mixed
     {
+        if (property_exists($this, $name)) {
+            return $this->{$name};
+        }
+
         $this->validateMetadataKey(str_replace(Resource::METADATA_PREFIX, '', $name));
 
         if (!str_contains($name, Resource::METADATA_PREFIX)) {
@@ -28,6 +62,10 @@ final readonly class BlobMetadata
 
     public function has(string $name): bool
     {
+        if (property_exists($this, $name)) {
+            return $this->{$name} !== null;
+        }
+
         $this->validateMetadataKey(str_replace(Resource::METADATA_PREFIX, '', $name));
 
         if (!str_contains($name, Resource::METADATA_PREFIX)) {
@@ -35,6 +73,24 @@ final readonly class BlobMetadata
         }
 
         return isset($this->metadata[$name]);
+    }
+
+    /** @return array<string, scalar> */
+    public function getMetadataToSave(): array
+    {
+        $metadata = [];
+
+        foreach ($this->metadata as $name => $value) {
+            $this->validateMetadataKey(str_replace(Resource::METADATA_PREFIX, '', $name));
+
+            $key = !str_starts_with($name, Resource::METADATA_PREFIX)
+                ? Resource::METADATA_PREFIX . $name
+                : $name;
+
+            $metadata[$key] = $value;
+        }
+
+        return array_filter($metadata, fn (mixed $value) => $value !== null);
     }
 
     protected function validateMetadataKey(string $key): void
