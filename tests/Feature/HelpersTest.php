0daeca364dacd18e62fb740d11a7bd1ad204bf89
Gabriel Ramos <gabrielramos791@gmail.com>
2024-07-27 15:30:21 -0300

    add new helpers functions

--- tests/Feature/HelpersTest.php
+++ tests/Feature/HelpersTest.php
@@ -1,5 +1,24 @@
 <?php
 
+uses()->group('helpers');
+
+it('should check with function', function () {
+    $called  = false;
+    $content = 'test';
+
+    expect(with($content, function (string $value) use (&$called, $content) {
+        $called = true;
+
+        expect($value)->toBe($content);
+    }))->toBe($content);
+
+    expect($called)->toBeTrue();
+});
+
+it('should check if it\'s running in console', function () {
+    expect(is_running_in_console())->toBeTrue();
+});
+
 it('should convert camel case string to be used in the headers', function (string $value, string $expected) {
     expect(str_camel_to_header($value))->toBe($expected);
 })->with([
@@ -23,3 +42,20 @@ it('should convert value to boolean type', function (mixed $value, bool $expecte
     'Object'       => [(object)['test' => 'test'], false],
     'Array'        => [[1, 2, 3], false],
 ]);
+
+it('should convert date time to RFC1123 format', function () {
+    $datetime = (new DateTime('2022-05-26 04:12:36', new DateTimeZone('Asia/Jakarta')));
+    $expected = (clone $datetime)->setTimezone(new DateTimeZone('GMT'));
+
+    expect(convert_to_RFC1123($datetime))->toBe("{$expected->format('D, d M Y H:i:s')} GMT");
+});
+
+it('should convert datetime to RFC3339 micro format', function () {
+    $datetime = (new DateTime('2024-08-10 12:04:59', new DateTimeZone('America/New_York')));
+    $expected = (clone $datetime)->setTimezone(new DateTimeZone('UTC'));
+
+    $microseconds = $datetime->format('u');
+    $microseconds = str_pad($microseconds, 7, '0', STR_PAD_LEFT);
+
+    expect(convert_to_RFC3339_micro($datetime))->toBe("{$expected->format('Y-m-d\TH:i:s')}.{$microseconds}Z");
+});
