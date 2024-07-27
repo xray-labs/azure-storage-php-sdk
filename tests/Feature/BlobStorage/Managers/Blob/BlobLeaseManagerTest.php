764514195972243d355366a8a57712609f1828fe
Silvio Pereira <sjpereira2000@gmail.com>
2024-07-27 16:45:47 -0300

    Create new tests for blob storage

---
+++ tests/Feature/BlobStorage/Managers/Blob/BlobLeaseManagerTest.php
@@ -0,0 +1,137 @@
+<?php
+
+use Xray\AzureStoragePhpSdk\Authentication\SharedKeyAuth;
+use Xray\AzureStoragePhpSdk\BlobStorage\Config;
+use Xray\AzureStoragePhpSdk\BlobStorage\Entities\Blob\BlobLease;
+use Xray\AzureStoragePhpSdk\BlobStorage\Managers\Blob\BlobLeaseManager;
+use Xray\AzureStoragePhpSdk\Exceptions\RequiredFieldException;
+use Xray\AzureStoragePhpSdk\Tests\Http\{RequestFake, ResponseFake};
+
+uses()->group('blob-storage', 'managers', 'blob');
+
+const REQUEST_URL = 'container/blob?comp=lease&resttype=blob';
+const VERSION     = '2020-06-12';
+
+it('should acquire a lease', function () {
+    ['request' => $request, 'manager' => $manager] = prepareForBlobLeaseManagerTest();
+
+    $response = $manager->acquire();
+
+    expect($response)
+        ->lastModified->toBeInstanceOf(DateTimeImmutable::class)
+        ->etag->toBe('KCSNSAKDMA')
+        ->server->toBe('MS-AZURE')
+        ->requestId->toBe('osjdnw-29389dksd-dwwdwd')
+        ->version->toBe(VERSION)
+        ->leaseId->toBe('lalsncjwej-29389dksd-dwwdwd')
+        ->date->toBeInstanceOf(DateTimeImmutable::class);
+
+    $request->assertPut(REQUEST_URL);
+});
+
+it('should renew a lease', function () {
+    ['request' => $request, 'blobLease' => $blobLease] = prepareForBlobLeaseManagerTest();
+
+    $response = $blobLease->renew();
+
+    expect($response)
+        ->lastModified->toBeInstanceOf(DateTimeImmutable::class)
+        ->etag->toBe('KCSNSAKDMA')
+        ->server->toBe('MS-AZURE')
+        ->requestId->toBe('osjdnw-29389dksd-dwwdwd')
+        ->version->toBe(VERSION)
+        ->leaseId->toBe('lalsncjwej-29389dksd-dwwdwd')
+        ->date->toBeInstanceOf(DateTimeImmutable::class);
+
+    $request->assertPut(REQUEST_URL);
+});
+
+it('should change a lease', function () {
+    ['request' => $request, 'blobLease' => $blobLease] = prepareForBlobLeaseManagerTest();
+
+    $response = $blobLease->change('lalsncjwej-29389dksd-dwwdwd');
+
+    expect($response)
+        ->lastModified->toBeInstanceOf(DateTimeImmutable::class)
+        ->etag->toBe('KCSNSAKDMA')
+        ->server->toBe('MS-AZURE')
+        ->requestId->toBe('osjdnw-29389dksd-dwwdwd')
+        ->version->toBe(VERSION)
+        ->leaseId->toBe('lalsncjwej-29389dksd-dwwdwd')
+        ->date->toBeInstanceOf(DateTimeImmutable::class);
+
+    $request->assertPut(REQUEST_URL);
+});
+
+it('should release a lease', function () {
+    ['request' => $request, 'blobLease' => $blobLease] = prepareForBlobLeaseManagerTest();
+
+    $response = $blobLease->release('lalsncjwej-29389dksd-dwwdwd');
+
+    expect($response)
+        ->lastModified->toBeInstanceOf(DateTimeImmutable::class)
+        ->etag->toBe('KCSNSAKDMA')
+        ->server->toBe('MS-AZURE')
+        ->requestId->toBe('osjdnw-29389dksd-dwwdwd')
+        ->version->toBe(VERSION)
+        ->leaseId->toBe('lalsncjwej-29389dksd-dwwdwd')
+        ->date->toBeInstanceOf(DateTimeImmutable::class);
+
+    $request->assertPut(REQUEST_URL);
+});
+
+it('should break a lease', function () {
+    ['request' => $request, 'blobLease' => $blobLease] = prepareForBlobLeaseManagerTest();
+
+    $response = $blobLease->break('lalsncjwej-29389dksd-dwwdwd');
+
+    expect($response)
+        ->lastModified->toBeInstanceOf(DateTimeImmutable::class)
+        ->etag->toBe('KCSNSAKDMA')
+        ->server->toBe('MS-AZURE')
+        ->requestId->toBe('osjdnw-29389dksd-dwwdwd')
+        ->version->toBe(VERSION)
+        ->leaseId->toBe('lalsncjwej-29389dksd-dwwdwd')
+        ->date->toBeInstanceOf(DateTimeImmutable::class);
+
+    $request->assertPut(REQUEST_URL);
+});
+
+it('should throw an exception when trying to renew a lease without a lease id', function () {
+    ['blobLease' => $blobLease] = prepareForBlobLeaseManagerTest(['x-ms-lease-id' => '']);
+
+    $blobLease->renew();
+})->throws(RequiredFieldException::class, 'Field [leaseId] is required');
+
+/**
+ * @param array<string, scalar> $blobLeaseHeaders
+ * @return array{request: RequestFake, blobLease: BlobLease, manager: BlobLeaseManager}
+ */
+function prepareForBlobLeaseManagerTest(array $blobLeaseHeaders = []): array
+{
+    $blobLeaseHeaders = array_merge([
+        'Last-Modified'   => 'Wed, 15 Sep 2021 15:02:29 GMT',
+        'ETag'            => 'KCSNSAKDMA',
+        'Server'          => 'MS-AZURE',
+        'Date'            => 'Wed, 15 Sep 2021 15:02:29 GMT',
+        'x-ms-request-id' => 'osjdnw-29389dksd-dwwdwd',
+        'x-ms-version'    => VERSION,
+        'x-ms-lease-id'   => 'lalsncjwej-29389dksd-dwwdwd',
+    ], $blobLeaseHeaders);
+
+    $request = (new RequestFake(new Config(new SharedKeyAuth('account', 'key'))))
+        ->withFakeResponse(new ResponseFake(headers: $blobLeaseHeaders));
+
+    // @phpstan-ignore-next-line
+    $blobLease = new BlobLease($blobLeaseHeaders);
+
+    $manager = (new BlobLeaseManager($request, 'container', 'blob'));
+
+    $blobLease->setManager($manager);
+
+    return [
+        'request'   => $request,
+        'blobLease' => $blobLease,
+        'manager'   => $manager,
+    ];
+}
---
+++ tests/Feature/BlobStorage/Queries/BlobTagQueryTest.php
@@ -0,0 +1,80 @@
+<?php
+
+use Xray\AzureStoragePhpSdk\Authentication\SharedKeyAuth;
+use Xray\AzureStoragePhpSdk\BlobStorage\Config;
+use Xray\AzureStoragePhpSdk\BlobStorage\Managers\Blob\BlobManager;
+use Xray\AzureStoragePhpSdk\BlobStorage\Queries\BlobTagQuery as QueriesBlobTagQuery;
+use Xray\AzureStoragePhpSdk\Exceptions\{InvalidArgumentException, RequiredFieldException};
+use Xray\AzureStoragePhpSdk\Tests\Http\{RequestFake};
+
+uses()->group('blob-storage', 'queries');
+
+it('should create a query', function () {
+    $request = (new RequestFake(new Config(new SharedKeyAuth('account', 'key'))));
+
+    $manager = (new BlobManager($request, 'container'));
+
+    $query = (new QueriesBlobTagQuery($manager))
+        ->where('tag', 'value')
+        ->where('sequence', '>', '2')
+        ->where('sequence', '<', '10');
+
+    expect((fn () => $this->wheres)->call($query))
+        ->toHaveCount(3)
+        ->toEqual([
+            ['tag' => 'tag', 'operator' => '=', 'value' => 'value'],
+            ['tag' => 'sequence', 'operator' => '>', 'value' => '2'],
+            ['tag' => 'sequence', 'operator' => '<', 'value' => '10'],
+        ]);
+});
+
+it('should set the whenBuild callback', function () {
+    $request = (new RequestFake(new Config(new SharedKeyAuth('account', 'key'))));
+
+    $manager = (new BlobManager($request, 'container'));
+
+    $query = (new QueriesBlobTagQuery($manager))
+        ->whenBuild(function (string $query): object {
+            return (object) ['query' => $query];
+        });
+
+    expect((fn () => $this->callback)->call($query))
+        ->toBeInstanceOf(Closure::class);
+});
+
+it('should throw an exception when the whenBuild callback is not set', function () {
+    $request = (new RequestFake(new Config(new SharedKeyAuth('account', 'key'))));
+
+    $manager = (new BlobManager($request, 'container'));
+
+    $query = (new QueriesBlobTagQuery($manager));
+
+    $query->build();
+})->throws(RequiredFieldException::class, 'Field [callback] is required');
+
+it('should build the query', function () {
+    $request = (new RequestFake(new Config(new SharedKeyAuth('account', 'key'))));
+
+    $manager = (new BlobManager($request, 'container'));
+
+    $query = (new QueriesBlobTagQuery($manager))
+        ->where('tag', 'value')
+        ->where('sequence', '>', '2')
+        ->where('sequence', '<', '10')
+        ->whenBuild(function (string $query): object {
+            return (object) ['query' => $query];
+        });
+
+    expect($query->build())
+        ->toEqual((object) ['query' => '%22sequence%22%3E%272%27AND%22sequence%22%3C%2710%27AND%22tag%22%3D%27value%27']);
+});
+
+it('should throw an exception when the operator is invalid', function () {
+    $request = (new RequestFake(new Config(new SharedKeyAuth('account', 'key'))));
+
+    $manager = (new BlobManager($request, 'container'));
+
+    $query = (new QueriesBlobTagQuery($manager));
+
+    $query->where('tag', 'invalid', 'value');
+})->throws(InvalidArgumentException::class, 'Invalid operator: invalid');
